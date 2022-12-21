2a9bb68e6441110b9a0284affef83c72a4f98e37
kumarvermaankit <kumarvermaankit9555@gmail.com>
2022-12-22 00:47:30 +0530

    push

---
+++ src/context/ChatProvider.js
@@ -0,0 +1,45 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState([]);
+  const [notification, setNotification] = useState([]);
+  const [score,setScore] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("sebUser"));
+    setUser(userInfo);
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <ChatContext.Provider
+      value={{
+        user,
+        setUser,
+        selectedChat,
+        setSelectedChat,
+        chats,
+        setChats,
+        notification,
+        setNotification,
+        score,
+        setScore
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
